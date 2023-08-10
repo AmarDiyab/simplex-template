787977692e6b79d56d3e82be83e3d1a16ee0b823
Amar Diyab <diyabamar3043@gmail.com>
2023-08-10 15:59:11 +0530

    first push

--- app/components/Stat.js
+++ app/components/Stat.js
@@ -1,30 +1,40 @@
 import Image from "next/image";
-import React from "react";
+import React, { useRef } from "react";
 import gifPoster from "../assets/gif-image.png";
 import styles from "../page.module.css";
-import overlay from '../assets/bg-overlay.svg';
+import { motion, useScroll, useTransform } from "framer-motion";
 
 const Stat = () => {
+    const ref = useRef(null);
+  useScroll({
+    target: ref,
+    offset: ['start start', 'end end']
+  })
+
   return (
     <div className="h-[400vh] min-h-[auto] flex flex-col relative justify-start top-[5%]  ">
-      <div className="sticky top-[5%]">
+      <div className="top-[5%] sticky">
         <div className="flex flex-wrap w-full justify-center px-[2.5em] overflow-visible">
-          <div className="flex items-start justify-end mt-[40px] relative overflow-hidden  ">
+          <div className="flex items-start justify-end mt-[40px] relative overflow-hidden ">
             <div className={styles.gif}>
-              <Image src={gifPoster} alt="gif-poster" className="relative h-[800px] overflow-hidden " />
-            <Image src={overlay} alt="overlay-image" className="inline-block align-middle overflow-clip" />
+              <Image
+                src={gifPoster}
+                style={{position: 'sticky'}}
+                alt="gif-poster"
+                className="relative h-[800px] overflow-hidden  "
+              />
             </div>
-
           </div>
         </div>
       </div>
 
-      <div className="flex flex-col absolute top-[0%] left-[6%] ">
-        <h1 className={styles.giftext}>5K</h1>
+
+      <motion.div  className="flex flex-col absolute top-[0%] left-[6%] ">
+        <h1 ref={ref} className={styles.giftext}>5K</h1>
         <div className="block">
           <h5 className={styles.giftextSub}>Completed Projects</h5>
         </div>
-      </div>
+      </motion.div>
 
       <div className="absolute flex flex-col top-[15%] right-[96px] ">
         <h1 className={styles.giftext}>65+</h1>
