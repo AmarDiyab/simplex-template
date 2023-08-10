aa518425ee3dec2e5ac3c7f8014cfc9c7a544670
Amar Diyab <diyabamar3043@gmail.com>
2023-08-10 13:49:16 +0530

    Stats update

--- app/components/test/stat-working.js
+++ app/components/test/stat-working.js
@@ -2,24 +2,21 @@ import Image from "next/image";
 import React from "react";
 import gifPoster from "../assets/gif-image.png";
 import styles from "../page.module.css";
-import overlay from '../assets/bg-overlay.svg';
 
 const Stat = () => {
   return (
     <div className="h-[400vh] min-h-[auto] flex flex-col relative justify-start top-[5%]  ">
-      <div className="sticky top-[5%]">
+      <div className="top-[5%] sticky">
         <div className="flex flex-wrap w-full justify-center px-[2.5em] overflow-visible">
           <div className="flex items-start justify-end mt-[40px] relative overflow-hidden  ">
             <div className={styles.gif}>
-              <Image src={gifPoster} alt="gif-poster" className="relative h-[800px] overflow-hidden " />
-            <Image src={overlay} alt="overlay-image" className="inline-block align-middle overflow-clip" />
+              <Image src={gifPoster} alt="gif-poster" className="relative h-[800px] overflow-hidden  " />
             </div>
-
           </div>
         </div>
       </div>
 
-      <div className="flex flex-col absolute top-[0%] left-[6%] ">
+      <div className="absolute flex flex-col top-0 left-[6%] ">
         <h1 className={styles.giftext}>5K</h1>
         <div className="block">
           <h5 className={styles.giftextSub}>Completed Projects</h5>
